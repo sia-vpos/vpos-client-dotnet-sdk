e30c1a8b383d03bc838a25b8fe5491e3c31b344a
dcolciaghi <d.colciaghi@reply.it>
2020-04-17 10:30:22 +0200

    html
    modified htmlFragment

--- VPOS-Library/Utils/HtmlTool.cs
+++ VPOS-Library/Utils/HtmlTool.cs
@@ -7,17 +7,16 @@ namespace VPOS_Library.Utils
     {
         private const string InputPattern = "<input type=\"hidden\" name=\"KEY\" value=\"VALUE\">";
 
-        private const string Template = "<div><form id=\"myForm\"action=\"[VPOS_URL]\" method=\"POST\"><input name=\"PAGE\" type=\"hidden\" value=\"[PAGE_VALUE]\">[PARAMETERS]</form><script>document.getElementById('myForm').submit();</script></div>";
-
+        private const string Template = "<div><form id=\"myForm\"action=\"[VPOS_URL]\" method=\"POST\"><input name=\"PAGE\" type=\"hidden\" value=\"[PAGE]\">[PARAMETERS]</form><script type=\"text/javascript\">function subForm() {document.getElementById('myForm').submit();}</script></div>";
         public string BuildHtml(string vposUrl, OrderedDictionary values)
         {
             var html = Template;
             if (values.Contains("TOKEN") && values["TOKEN"]  != null)
             {
-                html = html.Replace("[PAGE_VALUE]", "TOKEN");
+                html = html.Replace("[PAGE]", "TOKEN");
             }
             else {
-                html = html.Replace("[PAGE_VALUE]", "LAND");
+                html = html.Replace("[PAGE]", "LAND");
             }
             html = html.Replace("[VPOS_URL]", vposUrl);
             html = html.Replace("[PARAMETERS]", GenerateParamsHtml(values));  
